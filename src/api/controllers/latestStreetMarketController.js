422d974c8402c8a50e66a3bda2635ec336bd1b9d
maluvasc <mluisadav@gmail.com>
2022-10-23 19:18:47 -0300

    wip: fix

--- src/api/controllers/latestStreetMarketController.js
+++ src/api/controllers/latestStreetMarketController.js
@@ -3,13 +3,18 @@ const response = require("../mappers/latestStreetMarketResponse");
 class LatestStreetMarketController {
 	static async GetAllProductsFromtheHomePage(req, res) {
 		try {
-			const dataProvider1 = await database.Providers.findOne({
-				where: { id: 1 },
+			const dataProvider = await database.Providers.findAll({
+				attributes: [
+					["id", "id"],
+					["name", "name"],
+					["photo_url", "photo_url"],
+					["description", "description"],
+					["phone_number1", "phone_number1"],
+					["phone_number2", "phone_number2"]
+				],
 			});
-			const dataProvider2 = await database.Providers.findOne({
-				where: { id: 2 },
-			});
-			const allProductsOfProvider1 = await database.Products.findAll({
+
+			const allProducts = await database.Products.findAll({
 				attributes: [
 					["id", "id"],
 					["name", "name"],
@@ -18,32 +23,15 @@ class LatestStreetMarketController {
 					["alt_frontend_attribute", "alt"],
 					["product_weight", "subtitle"],
 					["price", "price"],
+					["provider_id","provider_id"], 
 				],
-				where: { provider_id: 1 },
-				limit: 5,
+				where:{ provider_id: dataProvider.dataValues.id},
 			});
-			const allProductsOfProvider2 = await database.Products.findAll({
-				attributes: [
-					["id", "id"],
-					["name", "name"],
-					["photo_url", "image"],
-					["type_frontend_attribute", "type"],
-					["alt_frontend_attribute", "alt"],
-					["product_weight", "subtitle"],
-					["price", "price"],
-				],
-				where: { provider_id: 2 },
-			});
+
+			const newData = dataProvider.map((data) => response(data, allProducts));
 			return res
 				.status(200)
-				.json(
-					response(
-						dataProvider1,
-						dataProvider2,
-						allProductsOfProvider1,
-						allProductsOfProvider2
-					)
-				);
+				.json(newData);
 		} catch (error) {
 			return res.status(500).json(error.message);
 		}
