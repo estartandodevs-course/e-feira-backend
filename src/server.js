923a6f44fe4f1737b55eb06929ac08a040ab26c5
maluvasc <mluisadav@gmail.com>
2022-11-03 15:52:09 -0300

    fix: healthcheck version

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(products, providers, highlights, category, streetMarket, user, order);
 app.get("/api", (req, res) => {
 	res.status(200).send({
 		message: "Hi! Welcome to the API",
-		version: "1.2.1",
+		version: "1.2.2",
 		date: new Date(),
 	});
 });
